2f35aae018475b48568528af07dd9b5259e00284
balbini <balbini91@gmail.com>
2017-09-29 15:19:31 -0700

    add pet working

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,9 +5,31 @@ class PetsController < ApplicationController
   end
 
   def show
+
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      redirect_to user_path(current_user.slug)
+    else
+      flash[:error] = "Your pet was not saved!"
+    end
   end
 
   def find_pet
     @pets = Pet.friendly.find(params[:slug])
   end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :breed, :age, :image, :about_me, :user_id)
+  end
+
+
 end
