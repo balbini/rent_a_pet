8c4bfa4e48bfb68ad41057b8108ffa99df1db78f
raybchoi <raybchoi@gmail.com>
2017-09-29 15:02:05 -0700

    working on user job creations

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,12 +1,28 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:secret]
+  before_action :find_job, only: [:edit, :show, :update]
 
   def index
-    @jobs = Job.all
   end
 
-  def show_all_jobs
-    @jobs = Job.all
+  def show
   end
-  
+
+  def new
+    @job = Job.new
+    explode
+  end
+
+  def create
+    explode
+    @job = Job.new(job_params)
+    if @job.save
+      @current.posts<<@post
+    end
+    redirect_to user_path(@user)
+  end
+
+  def find_job
+    @job = Job.find_by_id(params[:id])
+  end
 end
