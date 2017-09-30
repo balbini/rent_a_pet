5fff98699140cd09e8c6a122856b5906090f937b
raybchoi <raybchoi@gmail.com>
2017-09-29 22:16:37 -0700

    edit jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:secret]
-  before_action :find_job, only: [:edit, :show, :update]
+  before_action :find_job, only: [:edit, :show, :update, :destroy, :check_owner]
 
   def index
   end
@@ -14,11 +14,52 @@ class JobsController < ApplicationController
 
   def create
     @job = Job.new(job_params)
-    if @job.save
-      redirect_to job_path(@job)
+    if job_params["begin_date"].to_date < DateTime.now.to_date
+        flash[:error] = "Job cannot start in the past. Please fix the dates"
+        render new_job_path(@job)
     else
-      flash[:error] = "Your pet was not saved!"
+      if job_params["end_date"].to_date >= job_params["begin_date"].to_date
+        if @job.save
+          redirect_to job_path(@job)
+        else
+          flash[:error] = "Sorry! We ran into an error - could you try again?"
+        end
+      else
+        flash[:error] = "Job cannot end before the begin date. Please fix the dates and update again"
+        render new_job_path(@job)
+      end
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if job_params["begin_date"].to_date < DateTime.now.to_date
+        flash[:error] = "Job cannot start in the past. Please fix the dates"
+        render :edit
+    else
+      if job_params["end_date"].to_date >= job_params["begin_date"].to_date
+        if @job.save
+          @job.update_attributes(job_params)
+          redirect_to job_path(@job)
+        else
+          flash[:error] = "Sorry! We ran into an error - could you try again?"
+          render :edit
+        end
+      else
+        flash[:error] = "Job cannot end before the begin date. Please fix the dates and update again"
+        render :edit
+      end
+    end
+  end
+
+  def destroy
+    owner_slug = @job.owner.slug
+    @job.destroy
+    redirect_to user_path(owner_slug)
+    # once path is set use the path below
+    # redirect_back(fallback_location: root_path)
   end
 
   def find_job
@@ -28,26 +69,14 @@ class JobsController < ApplicationController
 
   private
   def job_params
-    params.require(:job).permit(:title, :description, :owner_id, :city, :pet_id, :begin_date, :end_date)
+    params.require(:job).permit(:title, :description, :owner_id, :city, :pet_id, :begin_date, :end_date, :status)
   end
-    # t.string "title"
-    # t.string "description"
-    # t.string "zip"
-    # t.date "begin_date"
-    # t.date "end_date"
-    # t.integer "dollar_value"
-    # t.string "type_of_job"
-    # t.string "status"
-    # t.string "recurring"
-    # t.string "pet_location"
-    # t.date "expiration_date"
-    # t.datetime "created_at", null: false
-    # t.datetime "updated_at", null: false
-    # t.string "address"
-    # t.string "city"
-    # t.string "state"
-    # t.integer "owner_id"
-    # t.integer "freelancer_id"
-    # t.bigint "pet_id"
 
+  def check_owner
+  # both are the same thing current_user.id == params[:id] and session[:id] === params[:id] => only if these are true then it allows the method to work for that SPECIFIC PARAMETER IN THE URL
+  # if the person that is logged is not the same as the persons data we are looking at ... flash a message or redirect_to their own page
+    if session[:user_id].to_s != @job.user.id.to_s
+      redirect_to user_path(current_user)
+    end
+  end
 end
