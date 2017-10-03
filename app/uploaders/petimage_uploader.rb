972846b27ed949bd9972191c5983be496a79eb60
raybchoi <raybchoi@gmail.com>
2017-10-03 10:50:38 -0700

    carrier wave and typeahead

--- app/uploaders/petimage_uploader.rb
+++ app/uploaders/petimage_uploader.rb
@@ -1,4 +1,4 @@
-class ProfileUploader < CarrierWave::Uploader::Base
+class PetimageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
@@ -15,28 +15,28 @@ class ProfileUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url(*args)
-    # For Rails 3.1+ asset pipeline compatibility:
-    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default_user_no_image.png"].compact.join('_'))
-
-    # "/images/fallback/" + [version_name, "default_user_no_image.png"].compact.join('_')
-  end
+  # def default_url
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
-  # def scale(200, 200)
-  #   process scale: [200, 200]
+  # def scale(width, height)
+  #   # do something
   # end
 
   # Create different versions of your uploaded files:
   # version :thumb do
   #   process resize_to_fit: [50, 50]
   # end
-
   version :thumb do
     process resize_to_fill: [280, 280]
   end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
